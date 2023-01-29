81155ebe2da88e3300d5477bc62ffab9582e435b
jcastro197 <jcastro2022@fau.edu>
2023-01-28 22:48:42 -0500

    Made complete upload service

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const express = require('express');
 const fileUpload = require('express-fileupload');
 const path = require('path');
 
+const filesPayloadExists = require('./middleware/filesPayloadExists');
+const fileExtLimiter = require('./middleware/fileExtLimiter');
+const fileSizeLimiter = require('./middleware/fileSizeLimiter');
+
 const PORT = process.env.PORT || 3500;
 
 const app = express();
@@ -13,11 +17,23 @@ app.get('/', (req, res) => {
 });
 
 app.post('/upload',
-    fileUpload({createParentPath: true}),
+    fileUpload({ createParentPath: true }),
+    filesPayloadExists,
+    fileExtLimiter(['.png', '.jpg', '.jpeg']),
+    fileSizeLimiter,
     (req, res) => {
-        const file = req.files;
+        const files = req.files
         console.log(files)
-        return res.json({status:'success', message: 'File uploaded successfully'});
-    });
+
+        Object.keys(files).forEach(key => {
+            const filepath = path.join(__dirname, 'files', files[key].name)
+            files[key].mv(filepath, (err) => {
+                if (err) return res.status(500).json({ status: "error", message: err })
+            })
+        })
+
+        return res.json({ status: 'success', message: Object.keys(files).toString() })
+    }
+)
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
