94393f3b502975ce686029d708b93ee5e958270b
jcastro197 <jcastro2022@fau.edu>
2023-01-30 16:32:27 -0500

    Fixed

--- app.js
+++ app.js
@@ -19,28 +19,37 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
+
+
 app.post('/upload',
     fileUpload({ createParentPath: true }),
     filesPayloadExists,
     fileExtLimiter(['.png', '.jpg', '.jpeg']),
     fileSizeLimiter,
     (req, res) => {
-        const files = req.files
-        files.mv(`${__dirname}/files/${files.name}`, (err) => {
-            if (err) return res.status(500).json({ status: "error", message: err })
-        })
-        return res.json({ status: 'success', message: Object.keys(files).toString() })
+
+        const files = req.files;
+        
+        // files.mv(`${__dirname}/files/${files.name}`, (err) => {
+        //     if (err) return res.status(500).json({ status: "error", message: err })
+        // })
+        // return res.json({ status: 'success', message: Object.keys(files).toString() })
 
         //console.log(files)
 
-        // Object.keys(files).forEach(key => {
-        //     const filepath = path.join(__dirname, 'files', files[key].name)
-        //     files[key].mv(filepath, (err) => {
-        //         if (err) return res.status(500).json({ status: "error", message: err })
-        //     })
-        // })
+        Object.keys(files).forEach(key => {
+            const filepath = path.join(__dirname, 'files', files[key].name)
+            filename = Object.keys(files).toString();
+            files[key].mv(filepath, (err) => {
+                if (err) return res.status(500).json({ status: "error", message: err })
+            })
+        })
 
-        // return res.json({ status: 'success', message: Object.keys(files).toString() })
+        
+
+        console.log(filename)
+
+        return res.json({ status: 'success', message: Object.keys(files).toString() })
     }
 );
 
@@ -61,10 +70,20 @@ app.post('/upload',
 
 
 app.get('/download', function(req, res){
-    const files1 = req.files
+
+    const files = req.file;
+
+    // Object.keys(files).forEach(key => {
+    //     const file = `${__dirname}/files/${files.name}`;
+    //     res.download(file); // Set disposition and send it.
+    //     files[key].mv(filepath, (err) => {
+    //         if (err) return res.status(500).json({ status: "error", message: err })
+    //     })
+    // })
     
-    const file = `${__dirname}/files/${files.name}`;
+    const file = `${__dirname}/files/${filename}`;
     res.download(file); // Set disposition and send it.
+
   });
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
